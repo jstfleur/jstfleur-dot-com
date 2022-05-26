1d55093b5bae3dc9916430308af7019a04dca206
jstfleur <jstfleur@ufl.edu>
2022-05-26 19:51:22 -0400

    UPDATE 1 LETS GO

--- js/script.js
+++ js/script.js
@@ -1,194 +1,57 @@
-function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
-}
-
-function pageTransition() {
-    var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-        duration: 1.2,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut",
-    });
-
-    tl.to(".loading-screen", {
-        duration: 1,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.3,
-    });
-    tl.set(".loading-screen", { left: "-100%" });
-}
-
-function contentAnimation() {
-    var tl = gsap.timeline();
-    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
-}
-
-$(function () {
-    barba.init({
-        sync: true,
-
-        transitions: [
-            {
-                async leave(data) {
-                    const done = this.async();
-
-                    pageTransition();
-                    await delay(1000);
-                    done();
-                },
-
-                async enter(data) {
-                    contentAnimation();
-                },
-
-                async once(data) {
-                    contentAnimation();
-                },
-            },
-        ],
-    });
-});
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
 
 
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("main section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+    
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        }, 2000);
 
 
-const cursor = document.querySelector('.cursor');
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300)
 
-const editCursor = e => {
-    const { clientX: x, clientY: y } = e;
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
-};
 
-window.addEventListener('mousemove', editCursor);
-
-const links = document.querySelectorAll('a, .hover-this')
-
-links.forEach(link => {
-    link.addEventListener('mouseenter', e => {
-        cursor.classList.add('enlarged')
     })
-    link.addEventListener('mouseout', e => {
-        cursor.classList.remove('enlarged')
-    })
-});
+})
 
 
-
-
-
-
-
-
-$(document).ready(function(){
-    $('.accordion-list > li > .answer').hide();
-    $('.imageContainer > li > img').hide();
-
-    
-    $('.accordion-list > li > h3').click(function() {
-        if ($(this).parent().hasClass("active")) {
-            $(this).parent().removeClass("active").find(".answer").slideUp();
-        } else {
-            $(".accordion-list > li.active .answer").slideUp();
-            $(".accordion-list > li.active").removeClass("active");
-            $(this).parent().addClass("active").find(".answer").slideDown();
-        }
-
-        return false;
-    });
-    
-    
-    $('.accordion-list > .project1 > h3').click(function() {
-        if ($(this).parent().hasClass("active")) {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project1 img").show();
-            
-        } else {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project1 img").hide();
-        }
-
-        return false;
-    });
-    
-    $('.accordion-list > .project2 > h3').click(function() {
-        if ($(this).parent().hasClass("active")) {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project2 img").show();
-            
-        } else {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project2 img").hide();
-        }
-
-        return false;
-    });
-    
-    $('.accordion-list > .project3 > h3').click(function() {
-        if ($(this).parent().hasClass("active")) {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project3 img").show();
-            
-        } else {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project3 img").hide();
-        }
-
-        return false;
-    });
-    
-    $('.accordion-list > .project4 > h3').click(function() {
-        if ($(this).parent().hasClass("active")) {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project4 img").show();
-            
-        } else {
-            $(this).parent().parent(".accordion-list").siblings(".imageContainer").find("li.project4 img").hide();
-        }
-
-        return false;
-    });
-    
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY;
   
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    mainNavLinks.forEach(link => {
+      let section = document.querySelector(link.hash);
+  
+      if (
+        section.offsetTop - 300 <= fromTop &&
+        section.offsetTop + section.offsetHeight - 300 > fromTop
+      ) {
+        link.classList.add("current");
+      } else {
+        link.classList.remove("current");
+      }
+    });
+  });
